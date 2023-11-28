45bb421d7f2610234120ef7869ca24ce270d041f
Stalkisiek <matmigdal@gmail.com>
2023-11-28 09:56:25 +0100

    Started react

--- SomeWebStuff/ts/index.ts
+++ SomeWebStuff/ts/index.ts
@@ -1,2 +1,49 @@
-let x:number = 3;
+interface Movie{
+    title: string;
+    year: number;
+}
+
+interface MovieFilter{
+    (m: Movie) : boolean;
+}
+
+class MoveDb{
+    private movies: Movie[];
+
+    constructor() {
+        this.movies = [];
+    }
+
+    addMovie(movie:Movie) : Movie{
+        this.movies.push(movie);
+        return movie;
+    }
+
+    addMovieByTitle(title: string, year: number) : Movie{
+        this.movies.push({title, year});
+        return {title, year};
+    }
+
+    movieFilter(func: MovieFilter){
+        console.log(this.movies.filter(func));
+    }
+}
+
+const obj: Movie = {
+    title: "abc",
+    year: 2003
+}
+
+const printMovie = (movie: Movie) => {
+    console.log(`title: ${movie.title} year: ${movie.year ?? "'"}`);
+}
+
+printMovie(obj);
+
+const movieDb = new MoveDb();
+movieDb.addMovieByTitle("Hi", 1999);
+movieDb.addMovie(obj);
+movieDb.movieFilter((movie) => (movie.year >= 1999));
+
+let x:number = 4;
 console.log(x);
